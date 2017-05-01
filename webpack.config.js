ef0e079cdec469af361ccf3cc861879abba69c59
Nikolay Krasko <goodwinnk@gmail.com>
2017-05-01 17:23:54 +0300

    Setup deploy directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
+function out(path) {
+    return 'deploy/' + path
+}
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-    entry: "./js/app.js",
+    entry: './js/app.js',
     output: {
-        filename: "bundle.js"
+        filename: out('bundle.js')
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: 'style!css' }
         ]
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'index.html', to: out('') },
+            { from: 'css/hollow_print.css', to: out('css') }
+        ])
+    ]
 };
